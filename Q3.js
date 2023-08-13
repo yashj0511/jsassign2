ac62e511001217e1c5feca56a4d53b852a2b9b48
Yash C Jaware <72185189+yashj0511@users.noreply.github.com>
2023-08-13 20:33:19 +0530

    Create Q3.js

---
+++ Q3.js
@@ -0,0 +1,24 @@
+function quickSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const pivot = array[array.length - 1];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] < pivot) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+const inputArray = [3, 2, 1, 4, 5, 45];
+const sortedArray = quickSort(inputArray);
+
+console.log("Output:", sortedArray);
